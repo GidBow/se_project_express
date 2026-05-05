c456f1a36fc54a95f0882d039ee770f9d343af7e
Gideon Bowman <GideonWBowman@gmail.com>
2026-05-05 14:56:26 -0700

    login

--- controllers/users.js
+++ controllers/users.js
@@ -107,12 +107,7 @@ const deleteUser = (req, res) => {
 
 const login = (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(BAD_REQUEST).send({
-      message: "Email and password are required",
-    });
-    return;
-  }
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
