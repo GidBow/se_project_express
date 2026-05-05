8d25c210ac0ac3028d727ea895469c97118b3ddd
Gideon Bowman <GideonWBowman@gmail.com>
2026-05-05 11:39:29 -0700

    syntax

--- controllers/users.js
+++ controllers/users.js
@@ -107,13 +107,13 @@ const deleteUser = (req, res) => {
 
 const login = (req, res) => {
   const { email, password } = req.body;
-     if (!email || !password) {
+  if (!email || !password) {
     res.status(BAD_REQUEST).send({
       message: "Email and password are required",
     });
     return;
   }
-    return User.findUserByCredentials(email, password)
+  User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: "7d",
